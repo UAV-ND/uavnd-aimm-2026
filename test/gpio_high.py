d08c5b3e6903a912cd1906abcae4eca6c16de764
uav nd <uavnd@nd.edu>
2026-03-17 18:37:04 -0400

    Updating dronekit fit our drone. Also, creating script to turn on pin for sulinoid drop

---
+++ test/gpio_high.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import Jetson.GPIO as GPIO
+import time
+
+# Use BOARD pin numbering (physical pins on the header)
+GPIO.setmode(GPIO.BOARD)
+
+# Choose the pin (example: pin 12)
+PIN = 12
+
+try:
+    # Setup pin as output
+    GPIO.setup(PIN, GPIO.OUT)
+
+    print(f"Setting pin {PIN} HIGH")
+
+    # Set pin HIGH
+    GPIO.output(PIN, GPIO.HIGH)
+
+    # Keep it HIGH for a while
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Stopping...")
+
+finally:
+    GPIO.output(PIN, GPIO.LOW)
+    GPIO.cleanup()
